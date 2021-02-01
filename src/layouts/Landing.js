aaa4984c17be498ef7b4ff55729ea3e741d4072d
Prashant Singh <singh.prashant13@tcs.com>
2021-02-01 11:27:33 +0530

    hell

--- src/layouts/Landing.js
+++ src/layouts/Landing.js
@@ -7,6 +7,7 @@ import Navbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 import Welcome from "views/welcome/Welcome.js";
 import Contact from "views/welcome/Contactus.js";
+import About from "views/welcome/About";
 
 export default function Landing() {
   return (
@@ -15,6 +16,7 @@ export default function Landing() {
       <main className="mt-15">
       <Switch>
         <Route path="/welcome" exact component={Welcome} />
+        <Route  path="/about" exact component={About} />
         <Route path="/welcome/contactus" exact component={Contact} />
         <Redirect from="/" to="/welcome" />
       </Switch>
