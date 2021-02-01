578861c2bb4f815b129210139ea30c84bf7686ca
Rahul <rahul.adsps@gmail.com>
2021-02-01 03:02:45 -0800

    api live

--- src/utility/constant/EndPoints.js
+++ src/utility/constant/EndPoints.js
@@ -1,5 +1,5 @@
 export const BASE_URL_LOCAL = 'http://ads.com/api/'
-export const BASE_URL_GLOBAL = ''
+export const BASE_URL_GLOBAL = 'http://api.kositutor.in/api/'
 export const PRESENT_TEACHERS = 'PresentTeachers'
 export const FOOTER = 'footer'
 export const LOGIN = 'token'
