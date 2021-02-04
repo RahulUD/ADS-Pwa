9334339fff4fe496b520ff88574deca267a15ec7
Rahul <rahul.adsps@gmail.com>
2021-02-03 21:17:19 -0800

    cc

--- src/utility/api/Contactus.js
+++ src/utility/api/Contactus.js
@@ -1,12 +1,8 @@
-import { CONTACTUS ,CONTACTUSPOST} from 'utility/constant/EndPoints'
-import Api from 'utility/api/Api'
+import { CONTACTUS} from "utility/constant/EndPoints";
+import Api from './Api'
 
 export default {
-  ContactUs(pageNo=1) {
-    return Api().get(CONTACTUS+pageNo)
-  },
-  ContactUsPost(payload) {
-    return Api().post(CONTACTUSPOST,payload)
+  submitContact(payload) {
+    return Api().post(CONTACTUS,payload)
   }
-
 }
