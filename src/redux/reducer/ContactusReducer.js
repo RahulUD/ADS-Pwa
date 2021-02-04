8fd56ba559ada85f97ae99d299eb9d9ecb5ea11b
Prashant Singh <singh.prashant13@tcs.com>
2021-02-04 11:15:56 +0530

    done1

--- src/redux/reducer/ContactusReducer.js
+++ src/redux/reducer/ContactusReducer.js
@@ -4,11 +4,15 @@ import {
   FAILURE_CONTACTUS,
 } from 'redux/ActionType'
 import {contactusInitialdata} from 'contract/contactus'
+import { REQUEST_CONTACTUS_POST } from 'redux/ActionType'
+import { RECEIVE_CONTACTUS_POST } from 'redux/ActionType'
+import { FAILURE_CONTACTUS_POST } from 'redux/ActionType'
 
 const initialState = {
   code: null,
   message: null,
   data: contactusInitialdata,
+  
   error: null,
 
 }
@@ -32,6 +36,24 @@ const ContactusReducer = (state = initialState, action) => {
 
         error: payload.error
       }
+      case REQUEST_CONTACTUS_POST:
+      return {
+        ...state,
+
+      }
+    case RECEIVE_CONTACTUS_POST:
+      return {
+        ...state,
+
+        message:"SUBMITTED SUCCESSFULLY"
+      }
+    case FAILURE_CONTACTUS_POST:
+      return {
+        ...state,
+        message:" NOT SUBMITTED SUCCESSFULLY",
+        error: payload.error
+      }
+    
     default:
       return {
         ...state
