e150674234487e3ecf57f6587950e1107a247926
Rahul <rahul.adsps@gmail.com>
2021-02-03 20:58:20 -0800

    message

--- src/redux/reducer/ContactusReducer.js
+++ src/redux/reducer/ContactusReducer.js
@@ -1,42 +1,42 @@
-import {
-  REQUEST_CONTACTUS,
-  RECEIVE_CONTACTUS,
-  FAILURE_CONTACTUS,
-} from 'redux/ActionType'
-import {contactusInitialdata} from 'contract/contactus'
+import { ENQUIRY_FAILURE } from "utility/constant/Message"
+import { ENQUIRY_SUCCESS } from "utility/constant/Message"
+import {REQUEST_CONTACTUS,
+    RECEIVE_CONTACTUS,
+    FAILURE_CONTACTUS
+  } from "../ActionType"
 
-const initialState = {
-  code: null,
-  message: null,
-  data: contactusInitialdata,
-  error: null,
-
-}
-const ContactusReducer = (state = initialState, action) => {
-  const { type, payload } = action
-  switch (type) {
-    case REQUEST_CONTACTUS:
-      return {
-        ...state,
-
-      }
-    case RECEIVE_CONTACTUS:
-      return {
-        ...state,
-
-        data: action.payload.data.data.contactus
-      }
-    case FAILURE_CONTACTUS:
-      return {
-        ...state,
-
-        error: payload.error
-      }
-    default:
-      return {
-        ...state
-      }
+  const initialState ={
+    code : null,
+    message : null,
+    error : null,
   }
-}
 
-export default ContactusReducer
+  const ContactUsReducer =(state=initialState, action) =>{
+    const {type, payload} =action
+    switch(type) {
+      case REQUEST_CONTACTUS:
+      return{
+        ...state,
+        
+      }
+      case RECEIVE_CONTACTUS:
+        return{
+          ...state,
+          
+        message:ENQUIRY_SUCCESS
+  
+        }
+        case FAILURE_CONTACTUS:
+          return{
+            ...state,
+            message:ENQUIRY_FAILURE,
+            error:payload.error
+          }
+      default:
+        return{
+          ...state
+        }
+
+    }
+  }
+  export default ContactUsReducer;
