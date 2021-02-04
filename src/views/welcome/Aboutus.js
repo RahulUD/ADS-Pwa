9d81b05c1339c6cca60eb84e13fd8c8c90766a79
Rahul <rahul.adsps@gmail.com>
2021-02-03 22:48:02 -0800

    contact form added

--- src/views/welcome/Aboutus.js
+++ src/views/welcome/Aboutus.js
@@ -2,6 +2,7 @@ import PresentTeachers from "components/Welcome/PresentTeachers";
 import React from "react";
 import HeaderStats from "components/Headers/HeaderStats.js";
 import sports from "assets/img/sports.jpg"
+import ContactUsForm from "components/Form/ContactUsForm";
 
 
 export default function Aboutus() {
@@ -9,59 +10,59 @@ export default function Aboutus() {
     <>
       <div className="justify-between pt-16 pb-16">
         <div className="bg-blue-500 shadow-md">
-        <div className="flex justify-between text-white container py-3 mx-auto py-3">
-          <div className="lg:w-12/12">
-            <p style={{ fontSize: '3em' }} className="mb-6 bold">ADS Public School</p>
-            <p style={{ fontSize: '1em' }}>2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently.</p>
-          </div>
+          <div className="flex justify-between text-white container py-3 mx-auto py-3">
+            <div className="lg:w-12/12">
+              <p style={{ fontSize: '3em' }} className="mb-6 bold">ADS Public School</p>
+              <p style={{ fontSize: '1em' }}>2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently. 2 days ago · Tailwind is one of the frameworks that take the approach of reusable, content-agnostic CSS, like other frameworks of the same type (Bootstrap, Foundation). This is obviously and as usual in the programming world a tradeoff. It does not care too much about separation of concerns, it aims to provide reusable CSS. In contrast to its competition, it tackles the subject differently.</p>
+            </div>
           </div>
         </div>
         <div className="shadow-md">
-        <div className="justify-between container mt-6 mx-auto">
-          <div className="lg:w-12/12 ">
-            <p style={{ fontSize: '2em' }} className="mb-6">From Directors Desk</p></div>
-          <div className="flex">
-            <div className="lg:w-4/12 px-2 py-2 shadow-md"><img className="float-left ..." src={sports} /></div>
-            <div className="lg:w-8/12 px-2 py-2"> React Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
-Either include it or remove the dependency array. However, 'props' w
-ill change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
-          </div></div>
-          </div>
-          <div className="shadow-md">
           <div className="justify-between container mt-6 mx-auto">
-          <div className="lg:w-12/12 ">
-            <p style={{ fontSize: '2em' }} className="mb-6">From Principal's Desk</p></div>
-          <div className="flex">
-            <div className="lg:w-4/12 px-2 py-2 shadow-md"><img className="float-left ..." src={sports} /></div>
-            <div className="lg:w-8/12 px-2 py-2"> React Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
-            Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+            <div className="lg:w-12/12 ">
+              <p style={{ fontSize: '2em' }} className="mb-6">From Directors Desk</p></div>
+            <div className="flex">
+              <div className="lg:w-4/12 px-2 py-2 shadow-md"><img className="float-left ..." src={sports} /></div>
+              <div className="lg:w-8/12 px-2 py-2"> React Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. However, 'props' w
+ill change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
+            </div></div>
+        </div>
+        <div className="shadow-md">
+          <div className="justify-between container mt-6 mx-auto">
+            <div className="lg:w-12/12 ">
+              <p style={{ fontSize: '2em' }} className="mb-6">From Principal's Desk</p></div>
+            <div className="flex">
+              <div className="lg:w-4/12 px-2 py-2 shadow-md"><img className="float-left ..." src={sports} /></div>
+              <div className="lg:w-8/12 px-2 py-2"> React Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
+              Either include it or remove the dependency array. HowevReact Hook useEffect has a missing dependency: 'props'.
 Either include it or remove the dependency array. However, 'props' will change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
-          </div></div>
-          </div>
+            </div></div>
+        </div>
         <HeaderStats />
         <div className="flex justify-center container py-3 py-3">
           <PresentTeachers />
         </div>
         <div className="flex justify-between container  mx-auto">
           <div className="lg:w-3/12 px-3 rounded-lg py-2 shadow-md">
-            <p style={{fontSize: '2em'}}>Vision</p>
+            <p style={{ fontSize: '2em' }}>Vision</p>
             React Hook useEffect has a missing dependency: 'props'.
 Either include it or remove the dependency array. However, 'props' will change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
           <div className="lg:w-3/12 px-3 rounded-lg py-2 shadow-md">
-          <p style={{fontSize: '2em'}}>Mission</p>
+            <p style={{ fontSize: '2em' }}>Mission</p>
             React Hook useEffect has a missing dependency: 'props'.
 Either include it or remove the dependency array. However, 'props' will change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
           <div className="lg:w-3/12 px-3 rounded-lg py-2 shadow-md">
-          <p style={{fontSize: '2em'}}>Motto</p>
+            <p style={{ fontSize: '2em' }}>Motto</p>
             React Hook useEffect has a missing dependency: 'props'.
 Either include it or remove the dependency array. However, 'props' will change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
           <div className="lg:w-3/12 px-3 rounded-lg py-2 shadow-md">
-          <p style={{fontSize: '2em'}}>Curriculum</p>
+            <p style={{ fontSize: '2em' }}>Curriculum</p>
             React Hook useEffect has a missing dependency: 'props'.
 Either include it or remove the dependency array. However, 'props' will change when *any* prop changes, so the preferred fix is to destructure the 'props' object outside of</div>
         </div>
@@ -129,75 +130,7 @@ Either include it or remove the dependency array. However, 'props' will change w
           </div>
         </div>
       </section>
-      <section className="relative block py-24 lg:pt-0 bg-gray-900">
-        <div className="container mx-auto px-4">
-          <div className="flex flex-wrap justify-center lg:-mt-64 -mt-48">
-            <div className="w-full lg:w-6/12 px-4">
-              <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-gray-300">
-                <div className="flex-auto p-5 lg:p-10">
-                  <h4 className="text-2xl font-semibold">
-                    Want to work with us?
-                    </h4>
-                  <p className="leading-relaxed mt-1 mb-4 text-gray-600">
-                    Complete this form and we will get back to you in 24
-                    hours.
-                    </p>
-                  <div className="relative w-full mb-3 mt-8">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="full-name"
-                    >
-                      Full Name
-                      </label>
-                    <input
-                      type="text"
-                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                      placeholder="Full Name"
-                    />
-                  </div>
-
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="email"
-                    >
-                      Email
-                      </label>
-                    <input
-                      type="email"
-                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full ease-linear transition-all duration-150"
-                      placeholder="Email"
-                    />
-                  </div>
-
-                  <div className="relative w-full mb-3">
-                    <label
-                      className="block uppercase text-gray-700 text-xs font-bold mb-2"
-                      htmlFor="message"
-                    >
-                      Message
-                      </label>
-                    <textarea
-                      rows="4"
-                      cols="80"
-                      className="px-3 py-3 placeholder-gray-400 text-gray-700 bg-white rounded text-sm shadow focus:outline-none focus:shadow-outline w-full"
-                      placeholder="Type a message..."
-                    />
-                  </div>
-                  <div className="text-center mt-6">
-                    <button
-                      className="bg-gray-900 text-white active:bg-gray-700 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      Send Message
-                      </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <ContactUsForm />
     </>
   )
 }
