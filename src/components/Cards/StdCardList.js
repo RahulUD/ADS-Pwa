9232b8bf36bd1a2522bd660667607f85faf0c55e
Rahul <rahul.adsps@gmail.com>
2021-02-04 23:26:44 -0800

    uploading to server

--- src/components/Cards/StdCardList.js
+++ src/components/Cards/StdCardList.js
@@ -8,7 +8,7 @@ const StdListCard = (props) => {
     }, [])
     return (
         <>
-            {props.stds?.data && props.stds.data.map(std => (<span className="text-xs font-semibold my-2 inline-block py-1 px-2 uppercase rounded text-blue-600 bg-blue-200 uppercase last:mr-0 mr-1" onClick={() => props.UpdateStdIdActionDispatch(std.id)}>
+            {props.stds?.data && props.stds.data.map(std => (<span className="cursor-pointer text-xs font-semibold my-2 inline-block py-1 px-2 uppercase rounded text-blue-600 bg-blue-200 uppercase last:mr-0 mr-1" onClick={() => props.UpdateStdIdActionDispatch(std.id)}>
                 {std.name} - {std.section}
             </span>))}
         </>
